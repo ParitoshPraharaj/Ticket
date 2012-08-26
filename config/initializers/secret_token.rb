cb11ffcb5b2f1a02602aab8ffef964d9a63d3b96
Paritosh Praharaj <parirakesh@gmail.com>
2012-08-26 20:43:45 +0530

    Installed rspec and cucumber generators.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticket::Application.config.secret_token = '93d7e6fbed2d0b007dcaf6005c18cb27c80aace7a441d9b05fbf4020c0a280b9970a3a75f7a15d99ddcbd8b5a64cfab224eb4ac63b2ba0a358b112b1c520cd6f'
